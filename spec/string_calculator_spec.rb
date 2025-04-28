ad6aabca2e7b455b1a3bc18d52cca3b2c2b8f31c
gowthamiroyalp <gowthamiroyalp@gmail.com>
2025-04-28 22:32:23 +0530

    test case for the non empty string sum

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(calculator.add("")).to eq(0)
     end
+
+    it 'returns sum of numbers only for non empty string' do
+      expect(calculator.add("34er")).to eq(7)
+      expect(calculator.add("32er ")).to eq(5)
+      expect(calculator.add("34er@@")).to eq(7)
+      expect(calculator.add("1,5")).to eq(6)
+    end
      
   end
 
--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,7 @@ class StringCalculator
 	def add(numbers)
 		# puts "numbers are: #{numbers} in addition calculation."
 		return 0 if numbers.empty?
+		return numbers.split('').map(&:to_i).sum
 	end
 
 end
