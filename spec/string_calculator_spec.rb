2043ab2452c722e3fc503ade62c4e2010ec932eb
gowthamiroyalp <gowthamiroyalp@gmail.com>
2025-04-28 23:55:12 +0530

    handles custom delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,7 +23,12 @@ RSpec.describe StringCalculator do
     it 'handles newlines as delimiters instead of comma, in between numbers' do
       expect(calculator.add("1\n2,3")).to eq(6)
     end
-         
+
+    it 'handles custom delimiter' do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
+
+     
   end
 
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -4,11 +4,17 @@ class StringCalculator
 		# puts "numbers are: #{numbers} in addition calculation."
 		return 0 if numbers.strip.empty?
 
-		# Replace newline with comma, then split by comma and map to to_i and sum
-    	numbers.gsub("\n", ",")
-	         .split(",")
-	         .map(&:to_i)
-	         .sum
+		delimiter = [",", "\n"] # Default delimiters
+
+	    if numbers.start_with?("//")
+	      delimiter = numbers[2]
+	      numbers = numbers.split("\n", 2)[1] # remove the first line
+	      numbers = numbers.split(delimiter)
+	    else
+	      numbers = numbers.split(/[\n,]/)
+	    end
+
+	    numbers.map(&:to_i).sum
 	end
 
 end
